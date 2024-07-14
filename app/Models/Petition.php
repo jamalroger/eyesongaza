0ecdb4a14a700be507a5eebc116400d56a968ed0
jamalroger <belharradi.j@gmail.com>
2024-07-14 21:47:47 +0100

    change db to psql

--- app/Models/Petition.php
+++ app/Models/Petition.php
@@ -3,8 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
-use MongoDB\Laravel\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
 class Petition extends Model
 {
     use HasFactory;
